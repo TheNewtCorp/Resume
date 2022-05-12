0061d4bb89c6a37b35db35ab9502f713feca15da
TheNewtCorp <thenewtcorp@gmail.com>
2022-05-12 17:51:00 -0400

    Second Commit

---
+++ hydrogen.config.js
@@ -0,0 +1,22 @@
+import {defineConfig} from '@shopify/hydrogen/config';
+import {
+  CookieSessionStorage,
+  PerformanceMetricsServerAnalyticsConnector,
+} from '@shopify/hydrogen';
+
+export default defineConfig({
+  routes: import.meta.globEager('./src/routes/**/*.server.[jt](s|sx)'),
+  shopify: {
+    storeDomain: 'newtcorp-informational-theme.myshopify.com',
+    storefrontToken: '4c3f327801b13aab40af2a5183a42c8e',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+  serverAnalyticsConnectors: [PerformanceMetricsServerAnalyticsConnector],
+});
